7388380dfc867afd9755f8823394a3c636449324
Liberess <58577796+Liberess@users.noreply.github.com>
2023-04-01 21:24:16 +0900

    FEAT Models

---
+++ Source/Colorverse/PaintedCollectObject.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "InteractObject.h"
+#include "PaintedCollectObject.generated.h"
+
+UCLASS(Blueprintable)
+class COLORVERSE_API APaintedCollectObject : public AInteractObject
+{
+	GENERATED_BODY()
+	
+};
