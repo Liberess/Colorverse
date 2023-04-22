48062da094d684db1e09e59d386500cdc62cdcb8
Liberess <58577796+Liberess@users.noreply.github.com>
2023-04-22 19:31:46 +0900

    Remove QuestComponent

---
+++ Source/Colorverse/Tooltip.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Tooltip.generated.h"
+
+UCLASS()
+class COLORVERSE_API UTooltip : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
