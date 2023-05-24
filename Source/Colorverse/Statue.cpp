be903221e5e645b0e62b18507481c7c352a416d0
Liberess <58577796+Liberess@users.noreply.github.com>
2023-05-24 18:20:21 +0900

    Update Statue.cpp

--- Source/Colorverse/Statue.cpp
+++ Source/Colorverse/Statue.cpp
@@ -21,9 +21,6 @@ void AStatue::OnInteract_Implementation()
 	bIsUnlock = true;
 	IsInteractable = false;
 	StaticMesh->SetRenderCustomDepth(false);
-
-	GEngine->AddOnScreenDebugMessage(-1, 1.0f, FColor::Orange,
-		TEXT("AStatue::OnInteract 실제 효과 적용 필요"));
 }
 
 void AStatue::OnExit_Implementation()
